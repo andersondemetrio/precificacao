991ae98c6974d28d39a5f6309f96ba7c096e3f83
Thiago Ruan Costa <thiagorcosta.26@gmail.com>
2023-10-16 23:05:43 -0300

    Adicionando permissionamento

--- static/assets/js/dre.js
+++ static/assets/js/dre.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var ctx = document.getElementById('myPieChart').getContext('2d');
+// document.addEventListener("DOMContentLoaded", function () {
+//     var ctx = document.getElementById('myPieChart').getContext('2d');
 
-    var totalTributos = parseFloat(document.getElementById('myPieChart').getAttribute('data-tot-tributos'));
-    var totalLucros = parseFloat(document.getElementById('myPieChart').getAttribute('data-tot-lucros'));
+//     var totalTributos = parseFloat(document.getElementById('myPieChart').getAttribute('data-tot-tributos'));
+//     var totalLucros = parseFloat(document.getElementById('myPieChart').getAttribute('data-tot-lucros'));
 
-    if (!isNaN(totalTributos) && !isNaN(totalLucros)) {
-        var data = {
-            labels: ['Impostos', 'Lucro'],
-            datasets: [{
-                data: [totalTributos, totalLucros],
-                backgroundColor: ['rgba(255, 99, 132, 0.6)', 'rgba(75, 192, 192, 0.6)']
-            }]
-        };
+//     if (!isNaN(totalTributos) && !isNaN(totalLucros)) {
+//         var data = {
+//             labels: ['Impostos', 'Lucro'],
+//             datasets: [{
+//                 data: [totalTributos, totalLucros],
+//                 backgroundColor: ['rgba(255, 99, 132, 0.6)', 'rgba(75, 192, 192, 0.6)']
+//             }]
+//         };
 
-        var myPieChart = new Chart(ctx, {
-            type: 'pie',
-            data: data,
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-                    display: true,
-                    text: 'Impostos vs. Lucro'
-                }
-            }
-        });
-    }
-});
+//         var myPieChart = new Chart(ctx, {
+//             type: 'pie',
+//             data: data,
+//             options: {
+//                 responsive: true,
+//                 maintainAspectRatio: false,
+//                 title: {
+//                     display: true,
+//                     text: 'Impostos vs. Lucro'
+//                 }
+//             }
+//         });
+//     }
+// });
